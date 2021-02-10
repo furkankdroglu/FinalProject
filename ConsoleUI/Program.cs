3c0c46210a4ea1353c03a6c40af9333759c1dcfc
furkankdroglu <75178233+furkankdroglu@users.noreply.github.com>
2021-02-10 17:03:34 +0300

    10 şubat 2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,12 +9,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager=new ProductManager(new EFProductDal());
+            //ProductTest();
+            CategoryManager categoryManager=new CategoryManager(new EFCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
 
-            foreach (var product in productManager.GetByUnitPrice(50,100))
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
+
             Console.WriteLine("Hello World!");
         }
     }
